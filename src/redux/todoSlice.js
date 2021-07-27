2501ed6a3f6438430e2c5f962c2248451249ecdc
mohsen.zergani <mohsen.zergani@gmail.com>
2021-07-27 17:09:11 +0430

    compelete todolist

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -16,8 +16,15 @@ const todoSlice = createSlice({
       };
       state.push(newTodo);
     },
+    toggleComplete: (state, action) => {
+      const index = state.findIndex((todo) => todo.id === action.payload.id);
+      state[index].completed = action.payload.completed;
+    },
+    deleteTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload.id);
+    },
   },
 });
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, toggleComplete, deleteTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
